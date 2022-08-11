b1e68831b769cf37f7c43db124a4a9ac601167a5
gamrom <gamrom01@naver.com>
2022-08-11 11:57:04 +0900

    test ok

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,23 @@ import styles from "../styles/Home.module.css";
 import axios from "axios";
 import { useState, useEffect } from "react";
 
-export default function Home(props) {
+const Home = (props) => {
 	console.log(props.data);
 
-	return <div>hi</div>;
-}
+	return props?.data?.map((anime) => {
+		return (
+			<div key={anime.mal_id}>
+				<h1>{anime.title}</h1>
+			</div>
+		);
+	});
+};
 
 export async function getServerSideProps(context) {
-	const res = await axios.get("https://api.jikan.moe/v4/anime/");
+	const res = await axios.get("https://api.jikan.moe/v4/anime/", {
+		params: { page: 1, limit: 10 },
+	});
 	return { props: res.data };
 }
+
+export default Home;
